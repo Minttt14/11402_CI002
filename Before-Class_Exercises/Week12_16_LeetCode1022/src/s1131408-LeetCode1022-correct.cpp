cad92709f94469f934419c8190e96f318cdc5892
Minttt14 <Xuan.cyo26@gmail.com>
2026-06-16 22:43:39 +0800

    [LeetCode] [1022] - [Sum of Root To Leaf Binary Numbers]

---
+++ Before-Class_Exercises/Week12_16_LeetCode1022/src/s1131408-LeetCode1022-correct.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int sumRootToLeaf(TreeNode* root){
+        return dfs(root, 0);
+    }
+
+private:
+    int dfs(TreeNode* node, int currentSum){
+        if (node == nullptr) {
+            return 0;
+        }
+        
+        currentSum = currentSum * 2 + node->val; 
+
+        if(node->left == nullptr && node->right == nullptr){
+            return currentSum;
+        }
+
+        return dfs(node->left, currentSum) + dfs(node->right, currentSum);
+    }
+};
